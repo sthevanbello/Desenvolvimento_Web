97395cc608d279f592c94f524537108aa0d90cac
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-10-24 13:14:57 -0300

    Aula 05 - JS

---
+++ scripts/index.js
@@ -0,0 +1,21 @@
+
+console.log("Aula de Javascript - Impacta")
+
+let idade = 35;
+console.log(idade);
+
+const objeto = {
+    "nome": "Sthevan",
+    "sobrenome": "Bello"
+}
+
+objeto.idade = idade
+console.log(objeto)
+console.log(objeto.nome)
+console.log(objeto.sobrenome)
+
+function somar(num1, num2){
+    return num1 + num2
+}
+
+console.log(somar(5,6))
